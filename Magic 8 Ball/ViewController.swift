1b495f8eff4301cdd8b1406c1cc64199808bdaf0
Greg Swan <gmswan99@gmail.com>
2023-09-03 10:11:41 -0400

    Update images for random selection

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let ballArray = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3") ]
+    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball3")]
     
     @IBOutlet weak var imageView: UIImageView!
         
