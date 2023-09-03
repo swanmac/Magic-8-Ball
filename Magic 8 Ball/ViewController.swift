09c3ddabd73c14748b038611f06046f50255eeb2
Greg Swan <gmswan99@gmail.com>
2023-09-03 10:01:43 -0400

    Add random action to ball selection

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
     
     let ballArray = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3") ]
+    
+    @IBOutlet weak var imageView: UIImageView!
         
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = ballArray[Int.random(in: 0...4)]
     
     
         
